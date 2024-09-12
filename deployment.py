19e38fd908da1c8aba755658206ca52c8fe2e7e7
emilyy541 <emilykuan6638@gmail.com>
2024-09-12 21:27:52 +0800

    Deployment.py

--- deployment.py
+++ deployment.py
@@ -18,18 +18,17 @@ st.write("""
     ### Enter the values for the following variables:
 """)
 
-# Inputs for features based on the scale of the variables
-feature_1 = st.number_input('Orthophosphate (mg/L)', format="%.3f")
-feature_2 = st.number_input('Ammonium (mg/L)', format="%.3f")
-feature_3 = st.number_input('Nitrite/Nitrate (mg/L)', format="%.3f")
-feature_4 = st.number_input('Chlorophyll (µg/L)', format="%.3f")
-feature_5 = st.number_input('Temperature (°C)', step=0.1)
-feature_6 = st.number_input('Salinity (Sal)', format="%.3f")
-feature_7 = st.number_input('Dissolved Oxygen (mg/L)', step=0.1)
-feature_8 = st.number_input('Depth (m)', step=0.1)
-feature_9 = st.number_input('pH', step=0.1)
-feature_10 = st.number_input('Turbidity', step=0.1)
-feature_11 = st.number_input('Chlorophyll Fluorescence', step=0.1)
+# Inputs without the increment buttons (step removed for free typing)
+feature_1 = st.text_input('Orthophosphate (mg/L)', value="0.00")
+feature_2 = st.text_input('Ammonium (mg/L)', value="0.00")
+feature_3 = st.text_input('Nitrite/Nitrate (mg/L)', value="0.0")
+feature_4 = st.text_input('Chlorophyll (µg/L)', value="0.0")
+feature_5 = st.text_input('Salinity (Sal)', value="0.0")
+feature_6 = st.text_input('Dissolved Oxygen (mg/L)', value="0.0")
+feature_7 = st.text_input('Depth (m)', value="0.0")
+feature_8 = st.text_input('pH', value="0.0")
+feature_9 = st.text_input('Turbidity (NTU)', value="0.0")
+feature_10 = st.text_input('Chlorophyll Fluorescence', value="0.0")
 
 # Define the threshold values
 thresholds = {
